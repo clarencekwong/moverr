c1f2d6f5634ee0371c6f81d8eb53f72fa4320ec1
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-11 16:53:03 -0500

    Worthless commit

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,12 @@
+# class Job < ApplicationRecord
+#   has_many :reviews
+#   has_many :furnitures
+#   has_many :jobs, through: :furnitures
+# end
+
+
+
 class Job < ApplicationRecord
-  has_many :reviews
-  has_many :furnitures
-  has_many :jobs, through: :furnitures
+  has_many :posters, class_name: 'User'
+  has_many :accepters, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
+# class User < ApplicationRecord
+#   has_many :furnitures
+#   has_many :jobs, through: :furnitures
+# end
+
+
 class User < ApplicationRecord
-  has_many :furnitures
-  has_many :jobs, through: :furnitures
+
+  # has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  # has_many :followees, through: :followed_users
+  #
+  # has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  # has_many :followers, through: :following_users
+
+
 end
