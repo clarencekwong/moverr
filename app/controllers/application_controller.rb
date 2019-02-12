be3136f9aeb4023d08478a20e089db280f739dd5
github username <clarencekwong7@gmail.com>
2019-02-12 17:58:35 -0500

    Application controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorized #lock down this whole app
+  helper_method :current_user #i can call current_user from a view
 
   def current_user
-    session[:user_id]
+    # User.find would throw an error if we cannot find the user
+    User.find_by({ id: session[:user_id] })
   end
 
-  private
+  def logged_in?
+    !!current_user
+  end
 
-  def require_logged_in
-    redirect_to controller: 'sessions', action: 'new' unless current_user
+  def authorized
+    redirect_to login_path unless logged_in?
   end
 end
