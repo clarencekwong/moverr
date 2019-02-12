74652edce0550839b09d72a8c827ab57643b1b9c
github username <clarencekwong7@gmail.com>
2019-02-12 18:25:27 -0500

    Logout functionality, and authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized, only: [:new, :create]
 
-  skip_before_action :authorized, only: [:new, :create]
-
   def new
     @user = User.new
   end
@@ -21,7 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
-    redirect_to '/login'
+    redirect_to '/'
   end
 
 end
