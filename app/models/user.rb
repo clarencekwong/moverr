1a3f1fa87d5fdb306333ef0b7d10e944c90090d4
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-12 09:30:16 -0500

    Added password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@
 
 
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :posted_jobs, class_name: "Job", foreign_key: "poster_id"
   has_many :mover_jobs, class_name: "Job", foreign_key: "mover_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_205441) do
+ActiveRecord::Schema.define(version: 2019_02_12_142539) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer "review_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "furniture_jobs", force: :cascade do |t|
     t.integer "furniture_id"
@@ -20,24 +28,26 @@ ActiveRecord::Schema.define(version: 2019_02_11_205441) do
   end
 
   create_table "furnitures", force: :cascade do |t|
+    t.string "name"
     t.string "category"
+    t.text "description"
+    t.integer "user_id"
+    t.integer "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
-    t.integer "poster_id"
-    t.integer "mover_id"
     t.datetime "date"
     t.string "status"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "job_id"
+    t.integer "user_id"
     t.text "content"
     t.integer "rating"
     t.datetime "created_at", null: false
@@ -46,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_02_11_205441) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "name"
     t.string "gender"
     t.string "address"
