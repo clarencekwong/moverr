84ca563962fbbe963229ba33219f178f0da7f872
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-13 08:15:54 -0500

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root :to => "mains#home"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   get '/self', to: 'users#myself', as: 'self'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
